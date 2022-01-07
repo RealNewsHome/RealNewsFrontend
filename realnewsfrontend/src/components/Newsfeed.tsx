dd4ccff0319ef7fbfa0194571d5cba40a2e90a1b
Caroline Spiezio <spieziocaroline@gmail.com>
2022-01-07 15:07:44 -0800

    adds homepage design blocks to feed

--- realnewsfrontend/src/components/Newsfeed.tsx
+++ realnewsfrontend/src/components/Newsfeed.tsx
@@ -13,10 +13,16 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios'
+import FeaturedPost from './Post'
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 interface Post {
   Title: string,
-  ID: number
+  ID: number,
+  Text: string
 }
 
 export default function Newsfeed() {
@@ -39,7 +45,27 @@ export default function Newsfeed() {
         posts.map((post: Post) => {
           console.log(post)
           return (
-            <p key={post.ID}>{post.Title}</p>
+            <Grid item xs={12} md={6} key={post.ID}>
+            <CardActionArea component="a" href="#">
+              <Card sx={{ display: 'flex' }}>
+                <CardContent sx={{ flex: 1 }}>
+                  <Typography component="h2" variant="h5">
+                    {post.Title}
+                  </Typography>
+                  <Typography variant="subtitle1" paragraph>
+                    {post.Text}
+                  </Typography>
+                  <Typography variant="subtitle1" color="primary">
+                    Continue reading...
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+                />
+              </Card>
+            </CardActionArea>
+          </Grid>
           )
         })
       }
