7c044a01135b1e3e74e4a1c399992d7d1313cf7c
Caroline Spiezio <spieziocaroline@gmail.com>
2022-02-12 19:44:14 -0500

    cleaner post page

--- realnewsfrontend/src/components/CreatePost.tsx
+++ realnewsfrontend/src/components/CreatePost.tsx
@@ -1,10 +1,88 @@
 import React, {useState, useContext, useEffect} from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { Link, useNavigate } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const NewPostForm = () => {
+  const token = window.localStorage.getItem('token');
+  if(!token) {
+    return (
+      <h1>Please Sign Up or Sign In to Post</h1>
+    )
+  }
+
+  // async function createPost
+  function Copyright(props: any) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const theme = createTheme();
+
+
   return (
-    <p>hi this is a new post page</p>
-  )
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+
+        <Grid item xs={12} sm={80} md={50} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="post"
+                label="Enter your post here:"
+                name="post"
+                autoComplete="post"
+                autoFocus
+                multiline={true}
+              />
+
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Submit Post
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
 
 export default NewPostForm
+
+//we need ... to check if token .. if there is a token, & curuser ... allow them to make a post request via axios .. create a form
